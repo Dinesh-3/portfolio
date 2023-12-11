af4016417c1a66ce6698008d56d94f13a7b68397
Dinesh I <dinesh.iyyandurai@vakilsearch.com>
2023-12-11 19:23:21 +0530

    Meta Head Component

--- src/components/MetaHead/index.jsx
+++ src/components/MetaHead/index.jsx
@@ -12,7 +12,6 @@ const MetaHead = (props) => {
       <link rel='icon' href='/favicon.ico' />
       <link rel='stylesheet' href='https://unicons.iconscout.com/release/v3.0.6/css/line.css' />
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
-      <link rel='stylesheet' href='/assets/css/swiper-bundle.min.css' />
       <link rel='stylesheet' href='/assets/css/styles.css' />
     </Head>
   );
