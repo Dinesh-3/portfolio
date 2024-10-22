3656aba5d6ef8d2195088c56469209bdaa999f46
Dinesh I <dinesh.iyyandurai@vakilsearch.com>
2024-10-22 10:25:10 +0530

    Content Update

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,4 +1,5 @@
 import clientPromise from '@/lib/mongodb';
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   const body = req.body;
@@ -13,6 +14,25 @@ export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db('portfolio');
     await db.collection('contacts').insertOne(data);
+  //   const mailOptions = {
+  //     from: `Dinesh <dinesh3it@gmail.com>`,
+  //    to:  'dinesh3it@gmail.com',
+  //     subject: `Job Opening From Company - Portfolio`,
+  //     html: `${JSON.stringify(data)}`
+  // };
+  // const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: 'dinesh3it@gmail.com',
+  //     pass: process.env.EMAIL_PASSWORD
+  //   }
+  //   });
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error)
+  //         return console.log(error);
+  //     console.log('Message sent: %s', info.messageId);
+  // });
+
     res.status(200).json({ status: true, message: 'Success' });
   } catch (error) {
     console.error('Error:', error);
